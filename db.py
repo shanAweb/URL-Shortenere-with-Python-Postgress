1f85d43c00eafd4dd64350de3e10553f3b50c341
shanAweb <shanasif2002@gmail.com>
2026-04-28 11:35:59 +0500

    Update: Loaded .env variables for DB connection

--- db.py
+++ db.py
@@ -1,4 +1,19 @@
-import psycopg2-binary
+# import psycopg2-binary
+import os
+from dotenv import load_dotenv
 
-psycopg2.connect()
+# Loading .env varaiables for DB credientiels
+DB_HOST = "DB_HOST"
+DB_PORT = "DB_PORT"
+DB_NAME = "DB_NAME"
+DB_USER = "DB_USER"
+DB_PASSWORD = "DB_PASSWORD"
+
+load_dotenv()
+print (os.getenv(DB_HOST, "DB host was not provided"))
+print (os.getenv(DB_NAME, "DB Name was not provided"))
+print (os.getenv(DB_PORT, "DB port was not provided"))
+print (os.getenv(DB_USER, "DB user was not provided"))
+print (os.getenv(DB_PASSWORD, "DB password was not required"))
+# print(os.getenv(""))
 
