aa0d86f0627b26bf52924a8c86b4f3fc933698d7
shanAweb <shanasif2002@gmail.com>
2026-05-06 12:07:06 +0500

    Update: Data is being fetched from the DB

--- db.py
+++ db.py
@@ -26,14 +26,26 @@ def save_url (original_url, short_code):
     
     db_connection = get_connection()
     cursor = db_connection.cursor()
+    # %s are placeholders, replaced with actual values. These are used to avoid SQL injection.
     cursor.execute("INSERT INTO urls(original_url, short_code) VALUES (%s, %s)", (original_url, short_code))
     db_connection.commit()
     cursor.close()
     db_connection.close()
 
+def get_original_url(short_code):
+    db_connection = get_connection()
+    cursor = db_connection.cursor()
+    # Reason for this , (short_code,) is that Python could treat it as tuple; because that is what psycopg2 expects
+    cursor.execute("SELECT original_url FROM urls WHERE short_code=(%s)", (short_code,))
+    result = cursor.fetchone()
+    url_result = result[0]
+    return url_result
+
 if __name__ == "__main__":
     save_url("https://www.psycopg.org/docs/index.html", "ab123")
     print("URL saved successfully!")
 
+    get_original_url("ab123")
+    print("Data Fetched successfully...")
 
 
